0d51821ed34c60b96e88e31ce4deae9f01a4285e
sarfarazkasmani <yusufkasmani491@gmail.com>
2024-06-16 19:17:59 +0530

    reomoved unwanted text

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -38,17 +38,7 @@ const Home = () => {
         <TabPanel header='By Category'>
           <Bycategory assets={assets} categories={categories} /> {/* [3] */}
         </TabPanel>
-        <TabPanel header='Header III'>
-          <p className='m-0'>
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui
-            blanditiis praesentium voluptatum deleniti atque corrupti quos
-            dolores et quas molestias excepturi sint occaecati cupiditate non
-            provident, similique sunt in culpa qui officia deserunt mollitia
-            animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis
-            est et expedita distinctio. Nam libero tempore, cum soluta nobis est
-            eligendi optio cumque nihil impedit quo minus.
-          </p>
-        </TabPanel>
+        <TabPanel header='Header III'>test</TabPanel>
       </TabView>
     </div>
   );
